b032aec6fed0db0f679cad7c55f39ca7e14647c1
cheezeman <pksam5046@naver.com>
2023-05-03 20:01:46 +0900

    create context

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { YoutubeApiProvider } from "../context/YoutubeApiContext";
 
 const queryClient = new QueryClient();
 export default function Root() {
   return (
     <>
       <NavBar />
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
     </>
   );
 }
