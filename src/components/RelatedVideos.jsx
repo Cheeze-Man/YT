4d0f0b279bdc4435698d723159615aa79a54f9b3
cheezeman <pksam5046@naver.com>
2023-05-04 12:34:19 +0900

    230504yt

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "./VideoCard";
 
 export default function RelatedVideos({ id }) {
-  return <div>{id}</div>;
+  const { youtube } = useYoutubeApi();
+  const {
+    error,
+    isLoading,
+    data: videos,
+  } = useQuery(["related", id], () => youtube.relatedVideos(id));
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😱</p>}
+      {videos && (
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
