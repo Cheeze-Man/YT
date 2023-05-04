14f1679affa8d4ccb20bce165c1cf0774d2ef212
cheezeman <pksam5046@naver.com>
2023-05-04 13:22:56 +0900

    Uncompleted ver.

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -9,15 +9,17 @@ export default function RelatedVideos({ id }) {
     error,
     isLoading,
     data: videos,
-  } = useQuery(["related", id], () => youtube.relatedVideos(id));
+  } = useQuery(["related", id], () => youtube.relatedVideos(id), {
+    staleTime: 1000 * 60 * 5,
+  });
   return (
     <div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 😱</p>}
       {videos && (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
+        <ul>
           {videos.map((video) => (
-            <VideoCard key={video.id} video={video} />
+            <VideoCard key={video.id} video={video} type="list" />
           ))}
         </ul>
       )}
